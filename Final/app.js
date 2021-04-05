567d0b1926baa5e05580287e9b8b172a20fc9236
jet342001 <64105418+jet342001@users.noreply.github.com>
2021-04-05 14:57:53 -0600

    final

--- Final/app.js
+++ Final/app.js
@@ -25,7 +25,3 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 });
 
-//simple add on to remove the main banner to start order
-function startOrder() {
-    document.querySelector('.hero').classList.toggle('start-order');
-}
---
+++ Final/js.js
@@ -0,0 +1,4 @@
+//simple add on to remove the main banner to start order
+function startOrder() {
+    document.querySelector('.hero').classList.toggle('start-order');
+}
